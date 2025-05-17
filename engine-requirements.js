1777555eecfa4d9b35474e3c0eb73a88068af643
Zero Developer <zeroaixz14@gmail.com>
2025-05-17 12:05:22 +0700

    Add files via upload

---
+++ engine-requirements.js
@@ -0,0 +1,10 @@
+const major = parseInt(process.versions.node.split('.')[0], 10);
+
+if (major < 18) {
+  console.error(
+    `\n❌ This package requires Node.js 18+ to run reliably.\n` +
+    `   You are using Node.js ${process.versions.node}.\n` +
+    `   Please upgrade to Node.js 18+ to proceed.\n`
+  );
+  process.exit(1);
+}
